87eca2168008c87d014e2e28dd4151652a797b48
danilogontijoSheridan <52791736+danilogontijoSheridan@users.noreply.github.com>
2019-07-18 11:31:03 -0400

    Add files via upload

---
+++ assets/data/myClasses.ts
@@ -0,0 +1,8 @@
+import{
+    ClassInfo
+} from "../../app/classInfo";
+
+export const MYCLASSES : ClassInfo[] = [
+    { cname : 'SYST24444', room : 'G202', dow: 'Thursday', tod : '9-11'},
+    { cname : 'SYST35300', room : 'B110', dow: 'Friday', tod : '2-5'}
+]
